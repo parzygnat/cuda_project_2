ff3fbfa6aa54455f991e5a0c92d8938d613929c0
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-17 00:07:33 +0100

    data generation with blobs

--- kmeans.cu
+++ kmeans.cu
@@ -14,6 +14,13 @@ struct Datum {
     double z{0};
 };
 
+struct Boundaries {
+    double upper_max{0};
+    double upper_min{0};
+    double lower_max{0};
+    double lower_min{0};
+};
+
 using Data = std::vector<Datum>;
 //TODO create a square function 
 
@@ -56,26 +63,63 @@ int main(int argc, char *argv[])
 {
     if(argc < 2)
     { 
-      printf("Not enough arguments\n 1st argument -> number of Datums to generate \n 2nd argument -> maximal absolute value on grid \n 3rd argument -> number of iterations of the k-means algorithm\n\n");
+      printf("Not enough arguments\n 1st argument -> number of examples to generate divisible by 8\n 2nd argument -> maximal absolute value on grid \n 3rd argument -> number of iterations of the k-means algorithm\n\n");
      return 0;
     }
-    size_t number_of_Datums = atoi(argv[1]);
+    //default number of clusters = 8;
+    size_t number_of_examples = atoi(argv[1]);
     double grid_max_value = atof(argv[2]);
     size_t number_of_iterations = atoi(argv[3]);
-    Data data(number_of_Datums);
+    Data data(number_of_examples);
+    Boundaries.upper_max = grid_max_value;
+    Boundaries.upper_min = grid_max_value*0.3;
+    Boundaries.lower_max = -grid_max_value;
+    Boundaries.lower_min = -grid_max_value*0.3;
     static std::random_device seed;
     static std::mt19937 random_number_generator(seed());
-    std::uniform_real_distribution<double> indices(-grid_max_value, grid_max_value);
-    for(auto& Datum : data) {
-        Datum.x = indices(random_number_generator);
-        Datum.y = indices(random_number_generator);
-        Datum.z = indices(random_number_generator);
+    std::uniform_real_distribution<double> indices_upper(Boundaries.upper_min, Boundaries.upper_max);
+    std::uniform_real_distribution<double> indices_lower(Boundaries.lower_min, Boundaries.lower_max);
+
+    for(int i = 0; i < number_of_examples; ++i) {
+        if(i < number_of_examples / 8){
+        Datum.x = indices_lower(random_number_generator);
+        Datum.y = indices_upper(random_number_generator);
+        Datum.z = indices_upper(random_number_generator);
+        } else if(i < 2*number_of_examples/8) {
+        Datum.x = indices_lower(random_number_generator);
+        Datum.y = indices_upper(random_number_generator);
+        Datum.z = indices_lower(random_number_generator);
+        } else if(i < 3*number_of_examples/8) {
+        Datum.x = indices_upper(random_number_generator);
+        Datum.y = indices_upper(random_number_generator);
+        Datum.z = indices_lower(random_number_generator);
+        } else if(i < 4*number_of_examples/8) {
+        Datum.x = indices_upper(random_number_generator);
+        Datum.y = indices_upper(random_number_generator);
+        Datum.z = indices_upper(random_number_generator);
+        } else if(i < 5*number_of_examples/8) {
+        Datum.x = indices_upper(random_number_generator);
+        Datum.y = indices_lower(random_number_generator);
+        Datum.z = indices_upper(random_number_generator);
+        } else if(i < 6*number_of_examples/8) {
+        Datum.x = indices_upper(random_number_generator);
+        Datum.y = indices_lower(random_number_generator);
+        Datum.z = indices_lower(random_number_generator);
+        } else if(i < 7*number_of_examples/8) {
+        Datum.x = indices_lower(random_number_generator);
+        Datum.y = indices_lower(random_number_generator);
+        Datum.z = indices_lower(random_number_generator);
+        } else if(i < number_of_examples) {
+        Datum.x = indices_lower(random_number_generator);
+        Datum.y = indices_lower(random_number_generator);
+        Datum.z = indices_upper(random_number_generator);
+        }
     }
     // Datum PRINTING
     for(auto& Datum : data) {
         printf("x is %f y is %f and z is %f \n", Datum.x, Datum.y, Datum.z);
     }
-    runCPU(data, number_of_Datums, number_of_iterations, grid_max_value);
+    runCPU(data, number_of_examples, number_of_iterations, grid_max_value);
     // runGpu();
     return 0;
 }
