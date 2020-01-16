8c4ad8f331d9d22623bb1be93955014bece391a2
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-16 23:03:28 +0100

    another using dataframe

--- kmeans.cu
+++ kmeans.cu
@@ -36,7 +36,7 @@ double squared_distance(Point a, Point b) {
 
 //         //TODO return means
 //     }
-
+using DataFrame = std::vector<Point>;
 int main(int argc, char *argv[])
 {
     if(argc < 2)
