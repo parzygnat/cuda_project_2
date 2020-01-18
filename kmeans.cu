c19b808fce7bb8fcb414d4a08d02e1aabe1bebb9
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-18 21:24:43 +0100

    work

--- kmeans.cu
+++ kmeans.cu
@@ -114,8 +114,11 @@ __global__ void distances_calculation(float* d_points_x, float* d_points_y, floa
         local_centroids[local_tid]= d_centroids_x[local_tid];
         local_centroids[local_tid + number_of_clusters]= d_centroids_y[local_tid];
         local_centroids[local_tid + number_of_clusters + number_of_clusters]= d_centroids_z[local_tid];
+        printf("im here\n");
     }
     __syncthreads();
+    if(local_tid < number_of_clusters && local_centroids[local_tid] != d_centroids_x[local_tid])
+        printf("NIE DZIALA\n");
     for(int i = 0; i < number_of_clusters; ++i) {
         const float _distance = distance_squared(_x, local_centroids[i], _y,local_centroids[i + number_of_clusters] , _z, local_centroids[i + 2*number_of_clusters]);
         if(_distance < currentDistance) {
